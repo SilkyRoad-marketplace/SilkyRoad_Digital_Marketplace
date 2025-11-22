7f263469091c4f04d4c14bdb8ca0595609580db8
SilkyRoad-marketplace <andytanth82@gmail.com>
2025-11-22 11:08:31 +0800

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,6 +1,6 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 export const supabase = createClient(
   "https://paqvgsruppgadgguynde.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.…"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.…" // your anon key
 );
